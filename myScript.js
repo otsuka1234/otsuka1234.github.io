1433137c708eabf4efea9f6dd13d31721995b30a
otsuka1234 <43218215+otsuka1234@users.noreply.github.com>
2018-10-23 21:50:34 -0500

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -200,3 +200,115 @@ function dropDown ()
     }
 
 }
+
+function tenToSmall (num,base)//convert a base- 10 number to a smaller base
+{
+// this function is expecting a number (num) in base 10
+    //it will convert the number (num) to value of 'base'
+    num= Number (document.toBinary.num1.value);//number to convert
+    base=Number (document.toBinary.num2.value);//destination base
+    smallNum=""; //store remainders here
+
+    while (num > 0)//continue looping while num isn't zero
+    {
+        smallNum = num%base + smallNum;// 3 %5 =2///% returns remainders
+        //setup num for the next iteration of the loop
+        num= Math.floor(num/base);//Floor rounds remainder down
+
+    }
+    return smallNum;
+}
+function smallToTen
+(num,base)//convert a small- base number to base-10\
+{
+    num= Number (document.toBase10.num1.value);
+    base=Number (document.toBase10.num2.value);
+
+    tenNum = 0; //stores number to be outputed
+    numLength = num.toString().length; //convert num to a STRING, then counts the number of characters in the string
+    // in this case, length of '1101' is 4
+    while (numLength > 0)
+    {
+        var denominator = Math.pow(10,(numLength - 1)); //gets denominator of fraction
+        // for 1101, denominators will be 1000,100,10,1
+
+        var currentDigit = Math.floor(num / denominator); //captures a 1 or 0 to multiply by 2^ numLength
+        //above means 10101/100 =1... 101/100 =1.... 01/10=0... 1/1=1...
+        tenNum = tenNum + currentDigit * Math.pow(base,numLength - 1);
+        //currentDigit*2^3 ... currentDigit *2^2....currentDigit*2^1... etc
+        num = num % denominator; //reduce num for the next iteration
+        //1101 -> 101 -> 01-> 1
+        numLength--; //decrease the numLength value by 1... otherwise will loop infintely
+    }
+    return tenNum;
+
+
+}
+
+
+function tenToSmall2 (num,base)//convert a base- 10 number to a smaller base
+{
+// this function is expecting a number (num) in base 10
+    //it will convert the number (num) to value of 'base'
+
+    temp1= smallToTen2(num,base);
+    base=Number(document.toAnyBase.base1.value);//destination base
+    smallNum=""; //store remainders here
+
+
+        while (temp1 > 0)//continue looping while num isn't zero
+        {
+            smallNum = temp1%base + smallNum;// 3 %5 =2///% returns remainders
+            //setup num for the next iteration of the loop
+            temp1= Math.floor(temp1/base);//Floor rounds remainder down
+
+        }
+        return smallNum;
+
+}
+function smallToTen2
+(num,base)//convert a small- base number to base-10\
+{
+    num = Number(document.toAnyBase.num1.value);
+    base = Number(document.toAnyBase.baseNum.value);
+
+    tenNum = 0; //stores number to be outputed
+    numLength = num.toString().length; //convert num to a STRING, then counts the number of characters in the string
+    // in this case, length of '1101' is 4
+
+    while (numLength > 0)
+    {
+
+        var denominator = Math.pow(10,(numLength - 1)); //gets denominator of fraction
+        // for 1101, denominators will be 1000,100,10,1
+
+        var currentDigit = Math.floor(num / denominator); //captures a 1 or 0 to multiply by 2^ numLength
+        //above means 10101/100 =1... 101/100 =1.... 01/10=0... 1/1=1...
+        tenNum = tenNum + currentDigit * Math.pow(base,numLength - 1);
+        //currentDigit*2^3 ... currentDigit *2^2....currentDigit*2^1... etc
+        num = num % denominator; //reduce num for the next iteration
+        //1101 -> 101 -> 01-> 1
+        numLength--; //decrease the numLength value by 1... otherwise will loop infintely
+    }
+    return tenNum;
+
+
+}
+function xAnyBase(num, x, y)
+{
+    num=Number(document.toAnyBase.num1.value);
+    x=Number(document.toAnyBase.baseNum.value);
+    y=Number(document.toAnyBase.base1.value);
+
+
+    var temp1= smallToTen2(num,x);
+    var temp2=tenToSmall2(temp1,y);
+    return temp2;
+
+}
+
+
+
+
+
+
